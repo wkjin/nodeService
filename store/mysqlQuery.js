21424b00d5c8712d30324b30e6d3acb3ba56e052
王堪进 <30312709+wkjin@users.noreply.github.com>
2018-08-25 07:46:38 +0800

    做了promise封装

--- store/mysqlQuery.js
+++ store/mysqlQuery.js
@@ -1,43 +1,113 @@
-const mysql = require( 'mysql' )
+const mysql  = require( 'mysql' );
 
-//建立连接池
-const pool = mysql.createPool({
+var IS_DEV = true;
+
+//定义返回数据的统一格式
+const getReturnData = function( error, msg, data ){
+    ( error === undefined ) && ( error = 1 );
+    return {
+        error,
+        msg: msg || '',
+        data: data || null
+    };
+};
+
+const setIsDev = function(status){
+    IS_DEV = (status === undefined) ? true : status;
+};
+
+
+//连接池配置文件
+var poolConfig = {
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'myfresh',
+    database: 'test_ionic',
     port: 3306,
-    connectionLimi: 100,
-    multipleStatements: true//允许一次执行多个sql
-})
+    connectionLimit: 10,
+    multipleStatements: true
+};
 
-//导出查询相关  
-const query = function( sql, callback ){    
-    pool.getConnection( function( err, conn ){    
-        if( err ){    
-            callback( err, null, null);    
-        }else{    
-            conn.query( sql, function( qerr, vals, fields ){    
-                //释放连接    
-                conn.release();    
-                //事件驱动回调    
-                callback( qerr, vals, fields);    
-            });    
-        }    
-    });    
-};    
+var pool = null;//保存连接池
 
-//获取一个列表
+//创建一个连接池
+const createMysqlPool = function(config){
+    if(pool === null){
+        pool = mysql.createPool(poolConfig);
+    }
+};
 
-//获取一条数据
+//断开pool
+const poolClose = function() {
+    return new Promise(( successCallback, errorCallback ) => {
+        if( pool !== null){
+            pool.end( error => {
+                if(error){
+                    errorCallback('关闭连接池失败：'+error.toString());
+                }else{
+                    pool = null;
+                    successCallback('已经关闭连接池');
+                }
+            });
+        }
+    });
+}
 
-//删除数据
 
-//修改数据
+//原生态查询，并返回结果集（使用Promise进行数据操作）
+const query = function(sql ){
+    createMysqlPool();
+    return new Promise( (successCallback, errorCallback ) => {
+        pool.getConnection(( error, conn ) => {
+            if( error ){
+                errorCallback( '数据库连接出错：' + error.toString());
+            }else{
+                conn.release(); //释放连接（防止运行结束无法释放连接）
+                conn.query( sql, function( err, results , fields ){
+                    if(err){
+                        errorCallback("查询出错：" + err.toString());
+                    }else{
+                        successCallback(results,fields);
+                    }
+                });
+            }
+        });
+    });
+};
 
-//添加数据
+//进行查询数据（按照）
+const sqlFetch = function( sql ){
+    return new Promise( callback => {
+        query(sql).then(( results, fields )=>{
+            var data = [];
+            switch(sql.trim().substr(0,6).toUpperCase()){
+                case 'SELECT':{
+                   data = JSON.parse(JSON.stringify(results));
+                }break;
+                case 'INSERT':{
+                   data['insertId'] = results.insertId;
+                   data['affectedRows'] = results.affectedRows
+                }break;
+                case 'UPDATE':{
+                   data['affectedRows'] = results.affectedRows
+                }break;
+                case 'DELETE':{
+                   data['affectedRows'] = results.affectedRows
+                }break;
+                default:{
+                    data = '数据库查询错误';
+                }
+            }
+            callback(getReturnData(0,'操作成功',data));
+        }).catch( error => {
+            callback( getReturnData( 1, IS_DEV ? error: '服务器错误' ));
+        });
+    });
+};
 
-module.exports= {
+module.exports = {
     query,
-
+    sqlFetch,
+    setIsDev,
+    poolClose
 }
